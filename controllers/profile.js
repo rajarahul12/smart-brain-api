3e561a2cffa46d4e9a72a85e8e4dba427c0a9ee8
Raja <maddula.rajarahul@gmail.com>
2018-12-20 13:37:29 +0530

    New Changes

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,21 +1,40 @@
-const handleProfileGet = (req,res,db)=>{
-	const{id}=req.params;
-	db.select('*').from('users').where({
-		id:id
-	})
-	.then(user => {
-		if(user.length){
-			res.json(user[0]);
-		}
-		else{
-			res.status(400).json('not found');
-		}
-	})
-	.catch(err =>{
-		res.status(400).json('error getting user');
-	})	
-}
+const handleProfileGet = (req, res, db) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({
+      id: id
+    })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("not found");
+      }
+    })
+    .catch(err => {
+      res.status(400).json("error getting user");
+    });
+};
 
-module.exports={
-	handleProfileGet : handleProfileGet
-}
+const handleProfileUpdate = (req, res, db) => {
+  // console.log(req.body.formInput);
+  const { id } = req.params;
+  const { name } = req.body.formInput;
+  db("users")
+    .where({ id })
+    .update({ name })
+    .then(resp => {
+      if (resp) {
+        res.json("success");
+      } else {
+        res.status(400).json("unable to update");
+      }
+    })
+    .catch(err => res.status(400).json("Error updating user"));
+};
+
+module.exports = {
+  handleProfileGet: handleProfileGet,
+  handleProfileUpdate: handleProfileUpdate
+};
--- server.js
+++ server.js
@@ -1,58 +1,103 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors=require('cors');
-const knex=require('knex');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const morgan = require("morgan");
 
 //Handlers Importing
-const register=require('./controllers/register');
-const signin=require('./controllers/signin');
-var profile=require('./controllers/profile');
-var image=require('./controllers/image');
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
-const db=knex({
-  client: 'pg',
+//Auth middleware for protecting routes / endpoints
+const auth = require("./controllers/authorization");
+
+/* For Heroku
+const db = knex({
+  client: "pg",
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl:true,
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
+});
+*/
+
+//For Localhost
+
+console.log("URI", process.env.POSTGRES_URI);
+
+const db = knex({
+  client: "pg",
+  connection: process.env.POSTGRES_URI
+  /*
+  connection: {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: "",
+    database: process.env.POSTGRES_DB
+  }*/
 });
 
 const app = express();
 
-//BODY PARSER
-app.use(bodyParser.json());
+const whitelist = ["http://localhost:3001"];
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
+
+//Applying Middlewares
+//MORGAN for logging
+app.use(morgan("combined"));
 //CORS
 app.use(cors());
+//BODY PARSER
+app.use(bodyParser.json());
 
-
-app.get("/",(req,res) => {
-	res.send('it is working');
+app.get("/", (req, res) => {
+  res.send("it is working");
 });
 
+// console.log("check");
 
 //Signing IN
-app.post("/signin", (req,res)=>{signin.handleSignin(req , res , db , bcrypt)});
+app.post("/signin", signin.signinAuthentication(db, bcrypt));
 
 //Registering
-app.post("/register",(req,res) => { register.handleRegister(req , res ,db , bcrypt) } );
-
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
 
 //Getting USER DATA for the Profile Page
-app.get('/profile/:id',(req,res) => { profile.handleProfileGet(req,res,db) });
+app.get("/profile/:id", auth.requireAuth, (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
 
+//Endpoint for Updating the user data
+app.post("/profile/:id", auth.requireAuth, (req, res) => {
+  profile.handleProfileUpdate(req, res, db);
+});
 
 //Increasing the entries
-app.put('/image',(req,res)=>{ image.handleImage(req,res,db)});
+app.put("/image", auth.requireAuth, (req, res) => {
+  image.handleImage(req, res, db);
+});
 
 //POST request for CLARIFAI API
-app.post('/imageUrl', (req,res) => { image.handleApiCall(req,res)})
+app.post("/imageUrl", auth.requireAuth, (req, res) => {
+  image.handleApiCall(req, res);
+});
 
-
-app.listen(process.env.PORT || 3000,()=>{
-	console.log(`app is running on port ${process.env.PORT}`);
-})
-
+app.listen(3000, () => {
+  console.log(`app is running on port 3000`);
+});
 
 /* --END POINTS
 / --> res = This is working
